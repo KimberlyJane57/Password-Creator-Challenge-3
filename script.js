a8978086cd36d300299d57b81549e4e2eaa0cdfa
KimberlyJane57 <kimberlythemotherofgod@gmail.com>
2022-12-18 14:42:49 -0700

    update prompts

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-// Assignment Code
-let generateBtn = document.querySelector("#generate");
+let characterLenth = 8;
 let choiceArray = [];
 
+let lowerCaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let upperCaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let numberCharacters = ['1','2','3','4','5','6','7','8','9','0'];
 let specialCharacters = ['!','@','#','$','%','^','&','*'];
-let lowerCaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-let upperCaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-let numberCharacters = ['1','2','3','4','5','6','7','8','9','0']
-
-function generatePassword() {
-  console.log ("Hey!")
-  //1. promt the user for the password criteria
-  //  a. password length between 8 & 128
-  //  b. lowercase, uppercase, number, special charaters
-  //2. Validate the input
-  //3. generate password based on criteria
-
-
-
-//4.Didspaly the generated password on the page
-  return "Generated password will go here."
-}
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(){
+  let promtsTf = getPromts();
+  
+  if(promtsTf){
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  }
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); {
+  //I will generatePassword based on the promts
+}
+
+function getPromts(){
+  choiceArray = [];
+
+  characterLenth = parseInt(prompt("Choose character length between 8 & 128"));
+
+  if(isNaN(characterLenth) || characterLenth < 8 || characterLenth > 128) {
+    alert("Needs to be a number, between 8 & 128 characters. Please try again.")
+    return false;
+  }
+  if(confirm("Would you like lowercase letters in your password?")){
+    choiceArray = choiceArray.concat(lowerCaseCharacters);
+  }
+  if(confirm("Would you like uppercase letters in your password?")){
+    choiceArray = choiceArray.concat(upperCaseCharacters);
+  }
+  if(confirm("Would you like numbers in your password?")){
+    choiceArray = choiceArray.concat(numberCharacters);
+  }
+  if(confirm("Would you like special charaters in your password?")){
+    choiceArray = choiceArray.concat(specialCharacters);
+  }
+  return true;
+}
