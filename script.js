7776bd24f284c8bc19ecad002e066c9ddaf84567
KimberlyJane57 <kimberlythemotherofgod@gmail.com>
2022-12-19 16:37:03 -0700

    updating functions

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  let promtsTf = getPromts();
+  let promptsTrue = getPromts();
   let passwordText = document.querySelector("#password");
-
-  if(promtsTf) {
+  
+  if(promptsTrue) {
     let newPassword = generatePassword();
     passwordText.value = newPassword;
   } else {
@@ -28,9 +28,10 @@ function writePassword() {
 function generatePassword() {
   let password = "";
   for(let i = 0; i < characterLenth; i++) {
-    let randomLetter = Math.floor(Math.random() * choiceArray.length);
-    password = password + choiceArray[randomLetter];
+    let randomletter = Math.floor(Math.random() * choiceArray.length);
+    password = password + choiceArray[randomletter];
   }
+  return password;
 }
 
 function getPromts() {
@@ -38,20 +39,20 @@ function getPromts() {
 
   characterLenth = parseInt(prompt("Choose character length between 8 & 128"));
 
-  if(isNaN(characterLenth) || characterLenth < 8 || characterLenth > 128) {
+  if (isNaN(characterLenth) || characterLenth < 8 || characterLenth > 128) {
     alert("Needs to be a number, between 8 & 128 characters. Please try again.");
     return false;
   }
-  if(confirm("Would you like lowercase letters in your password?")) {
+  if (confirm("Would you like lowercase letters in your password?")) {
     choiceArray = choiceArray.concat(lowerCaseCharacters);
   }
   if(confirm("Would you like uppercase letters in your password?")) {
     choiceArray = choiceArray.concat(upperCaseCharacters);
   }
-  if(confirm("Would you like numbers in your password?")) {
+  if (confirm("Would you like numbers in your password?")) {
     choiceArray = choiceArray.concat(numberCharacters);
   }
-  if(confirm("Would you like special charaters in your password?")) {
+  if (confirm("Would you like special charaters in your password?")) {
     choiceArray = choiceArray.concat(specialCharacters);
   }
   return true;
